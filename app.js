5f9eae1620d6d061febb32cbb308101dd1a69fd7
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-21 21:35:02 +0100

    vfs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //Router routes
