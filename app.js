10bca69058300c774ce12fad84727c3f20b2a81c
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-21 20:37:12 +0100

    vfs

--- app.js
+++ app.js
@@ -8,17 +8,10 @@ app.set('views', 'views');
 
 //Router routes
 const adminData = require('./routes/admin');
-const productRoutes = require('./routes/product');
 
-//Middleware - always run next()
-//Default response header is text/html for a string res
-//Need bodyParser to parse req.body
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'css')));
-//Imports from router
 app.use('/admin', adminData.routes);
-app.use(productRoutes);
-
 app.use((req, res, next) => {
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,10 @@ const  path = require('path');
 const express = require('express');
 const rootDir = require('../util/path');
 const router = express.Router();
-const products = [];
 
 router.get('/add-product', (req, res, next) => {
     res.render('addProduct')
 });
 
-router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/')
-});
-
 exports.routes = router;
-exports.products = products;
+
