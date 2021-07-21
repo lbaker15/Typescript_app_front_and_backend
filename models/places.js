8b031b87f954d05713378bb453ec43f9fd0a9d60
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-21 21:14:03 +0100

    vfs

--- models/places.js
+++ models/places.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Places = new Schema({
-    firstname: String
+    businessAddress: String,
+    lat: String, lng: String,
+    name: String,
+    telephone: Number
 })
 
 module.exports = mongoose.model('Places', Places)
