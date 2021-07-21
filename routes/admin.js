b405a9edd97f80d0d559e9c057d2069bcc2abf7b
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-21 21:22:37 +0100

    vfsd

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const controller = require('../controllers/places');
 
 router.post('/places', controller.getProviders);
+router.post('/add-place', controller.addProvider);
+
 
 exports.routes = router;
 
