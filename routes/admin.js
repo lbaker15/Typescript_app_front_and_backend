8bb7f561e3e280df7eca784a9a25e2bf6898c2e3
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-21 20:58:59 +0100

    vfs

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,9 @@ const path = require('path');
 const rootDir = require('../util/path');
 const express = require('express');
 const router = express.Router();
+const controller = require('./controllers/places');
 
-router.get('/', (req, res, next) => {
-    res.render('addProduct')
-});
+router.get('/', controller.getProviders);
 
 exports.routes = router;
 
