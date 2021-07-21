cc0b3795cd00fa93f92f7328f0ca754488c62d5d
lbaker15 <laelbaker@hotmail.co.uk>
2021-07-21 21:09:12 +0100

    vfs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
