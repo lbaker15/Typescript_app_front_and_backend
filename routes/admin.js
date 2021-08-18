bdb64b635c592a5c542a16fd3f6a6840f8ba0a4d
lbaker15 <laelbaker@hotmail.co.uk>
2021-08-18 17:40:41 +0100

    vfs

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const rootDir = require('../util/path');
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/places');
-const controller2 = require('../controllers/photos');
+const controller2 = require('../controllers/photo');
 const bucket = require('../middleware/upload');
 
 router.post('/places', controller.getProviders);
