3bfb1214acb1df161f2aa4284e2fe6e548111b83
lbaker15 <laelbaker@hotmail.co.uk>
2021-09-03 13:10:29 +0100

    vfs

--- controllers/places.js
+++ controllers/places.js
@@ -49,54 +49,54 @@ const getProviders = async (req, res, next) => {
 const addProvider = async (req, res, next) => {
     let { address, name, telephone, category, photo } = req.body;
     console.log(req.body)
-    if (address && name && telephone && category) {
-            let obj = {
-                name: String(name).toLowerCase(),
-                businessAddress: String(address).toLowerCase(),
-                telephone: Number(telephone),
-                category: String(category).toLowerCase()
-            }
-            let address2;
-            try {
-                address2 = await helpers.stringReplace(obj.businessAddress)
-            } catch(err) {
-                let error = new HttpError('Could not convert address to lowercase', 500)
-                return next(error);
-            }
-            let providerCoords;
-            try {
-                const {data} = await axios.post(`
-                    https://maps.googleapis.com/maps/api/geocode/json?address=${address2}&key=${key}
-                `)
-                providerCoords = await data.results[0].geometry.location; 
-                // setTimeout(async () => {
-                        if (providerCoords.lat && providerCoords.lng) {
-                            obj.lat = providerCoords.lat;
-                            obj.lng = providerCoords.lng;
-                            try {
-                                let add = new Places(obj)
-                                add.save().then((data, err) => {
+    // if (address && name && telephone && category) {
+    //         let obj = {
+    //             name: String(name).toLowerCase(),
+    //             businessAddress: String(address).toLowerCase(),
+    //             telephone: Number(telephone),
+    //             category: String(category).toLowerCase()
+    //         }
+    //         let address2;
+    //         try {
+    //             address2 = await helpers.stringReplace(obj.businessAddress)
+    //         } catch(err) {
+    //             let error = new HttpError('Could not convert address to lowercase', 500)
+    //             return next(error);
+    //         }
+    //         let providerCoords;
+    //         try {
+    //             const {data} = await axios.post(`
+    //                 https://maps.googleapis.com/maps/api/geocode/json?address=${address2}&key=${key}
+    //             `)
+    //             providerCoords = await data.results[0].geometry.location; 
+    //             // setTimeout(async () => {
+    //                     if (providerCoords.lat && providerCoords.lng) {
+    //                         obj.lat = providerCoords.lat;
+    //                         obj.lng = providerCoords.lng;
+    //                         try {
+    //                             let add = new Places(obj)
+    //                             add.save().then((data, err) => {
                                 
-                                    return res.json({'Data': data})
-                                })
-                            } catch(err) {
-                                let error = new HttpError('Could not save provider.', 500)
-                                return next(error);
-                            }
-                        } else {
-                            let error = new HttpError('Address fail, no provider coords.', 500)
-                            return next(error);
-                        }
-                    // }, 100)
-            } catch(err) {
-                console.log('ERR', err.response.data, err.response.headers)
-                let error = new HttpError('Address fail, address cannot be converted.', 500)
-                return next(error);
-            }
-        } else {
-            let error = new HttpError('Incomplete information form.', 422)
-            return next(error);
-        }
+    //                                 return res.json({'Data': data})
+    //                             })
+    //                         } catch(err) {
+    //                             let error = new HttpError('Could not save provider.', 500)
+    //                             return next(error);
+    //                         }
+    //                     } else {
+    //                         let error = new HttpError('Address fail, no provider coords.', 500)
+    //                         return next(error);
+    //                     }
+    //                 // }, 100)
+    //         } catch(err) {
+    //             console.log('ERR', err.response.data, err.response.headers)
+    //             let error = new HttpError('Address fail, address cannot be converted.', 500)
+    //             return next(error);
+    //         }
+    //     } else {
+    //         let error = new HttpError('Incomplete information form.', 422)
+    //         return next(error);
+    //     }
 }
 
 exports.getProviders = getProviders;
