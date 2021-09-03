41aa99ca146f3445787f09b92829500af6e5ad65
lbaker15 <laelbaker@hotmail.co.uk>
2021-09-03 13:23:40 +0100

    vfs

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'pug');
 app.set('views', 'views');
 app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 const adminData = require('./routes/admin');
 
 app.use(express.static(path.join(__dirname, 'css')));
--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const controller2 = require('../controllers/photo');
 const bucket = require('../middleware/upload');
 
 router.post('/places', controller.getProviders);
-router.post('/add-place', controller.addProvider);
+router.post('/add-place', bucket.fileUpload, controller.addProvider);
 router.post('/insta-upload', bucket.fileUpload, controller2.uploadPhoto);
 
 exports.routes = router;
