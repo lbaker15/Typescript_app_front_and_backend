d7608e57103765256eb0278dacb7f3dd7ca8d217
lbaker15 <laelbaker@hotmail.co.uk>
2021-09-04 11:30:03 +0100

    vfs

--- controllers/photo.js
+++ controllers/photo.js
@@ -22,7 +22,8 @@ const uploadPhoto = (req, response, next) => {
         const {username, password, message, time} = req.body;
         const {location} = req.file;
         let now = new Date().getTime();
-        let future = now + (Number(time)*1000);
+        console.log(now, time)
+        let future = Number(now) + (Number(time)*1000);
         let obj =  {"location": location, message, uploaded: false, username, password, time: future}
         
         let newItem = new ScheduledPhotos(obj);
